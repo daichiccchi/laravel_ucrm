0b34e956203d821ed8e68ae89f6d31fb792f45fd
青木大地 <daichi.aoki@alkaased.jp>
2022-12-30 16:07:22 +0900

    section2 done

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,20 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/components/InputLabel.vue'
+import Input from '@/components/TextInput.vue'
+
+const emitTest = (e) => {
+    console.log(e)
+}
+</script>
+
+<template>
+    <GuestLayout>
+        <Label value="件名">タイトル</Label>
+        <Input 
+            modelValue="初期値"
+            @update:modelValue="emitTest"
+        ></Input>
+    </GuestLayout>
+    
+</template>
---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Inertia } from '@inertiajs/inertia'
+// import ConfirmPassword from 'vendor/laravel/breeze/stubs/inertia-react/resources/js/Pages/Auth/ConfirmPassword';
+
+defineProps({
+    id : String,
+    blog: Object
+})
+
+const deleteConfirm = id => {
+    // console.log(id)
+    Inertia.delete(`/inertia/${id}`, {
+    onBefore: () => confirm('Are you sure you want to delete this user?'),
+})
+
+}
+</script>
+
+<template>
+    {{ id }}<br>
+    {{ blog.title }}<br>
+    <button @click="deleteConfirm(blog.id)">削除</button>
+</template>
